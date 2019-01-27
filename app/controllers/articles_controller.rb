c259da46db54ecab7e55cab3e5a00e9b7c31f0d4
ip-publishing <y.imai0929@gmail.com>
2019-01-27 23:21:42 +0900

    記事の作成
    記事の新規作成、他のページへのリンク貼り、エラーメッセージの出力など

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,29 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find params[:id]
+	end
+
 	def new
+		@article = Article.new
+	end
+
+	def create
+		#render plain: params[:articles].inspect
+		#raise params.inspect
+		@article = Article.new(article_params)
 		
+		if @article.save
+		  redirect_to article_path(@article)
+		else
+		  render 'new'
+		end
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :text)
 	end
 end
